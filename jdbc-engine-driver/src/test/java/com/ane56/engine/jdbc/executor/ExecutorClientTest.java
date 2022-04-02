2532a02b1362ea29f5813d5691ca86b468a6085f
xinsen <xinsen.zhang.0571@gmail.com>
2022-04-02 10:24:15 +0800

    update: async query in unit test

--- jdbc-engine-driver/src/test/java/com/ane56/engine/jdbc/executor/ExecutorClientTest.java
+++ jdbc-engine-driver/src/test/java/com/ane56/engine/jdbc/executor/ExecutorClientTest.java
@@ -9,7 +9,10 @@ import org.apache.thrift.TException;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.*;
 
 @Slf4j
 public class ExecutorClientTest {
@@ -24,21 +27,44 @@ public class ExecutorClientTest {
     }
 
     @Test
-    public void testQuery() throws InterruptedException, TException {
+    public void testExecutorQuery() throws InterruptedException, TException, ExecutionException {
         long startTime = System.currentTimeMillis();
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
         int maxIterationNum = 1000;
+        List<Future<JDBCResultRef>> jdbcResultRefFeatureList = new LinkedList<>();
         for (int i = 0; i < maxIterationNum; i++) {
+            Future<JDBCResultRef> jdbcResultRefFuture = executorService.submit(new AsyncQuery());
+            jdbcResultRefFeatureList.add(jdbcResultRefFuture);
+        }
+        for (Future<JDBCResultRef> jdbcResultRefFuture : jdbcResultRefFeatureList) {
+            JDBCResultRef jdbcResultRef = jdbcResultRefFuture.get();
+        }
+        System.out.println(System.currentTimeMillis() - startTime);
+    }
+
+    public class AsyncQuery implements Callable<JDBCResultRef> {
+
+        @Override
+        public JDBCResultRef call() {
             JDBCEngineExecutorRef engineExecutorRef = JDBCEngineExecutorRef.builder()
                     .executorRefId(UUID.randomUUID())
-                    .host("192.168.66.117")
+                    .host("192.168.66.75")
                     .port(8889)
                     .build();
             String querySQL = "select * from tx_dev.bd_center_center_month;";
 //            String querySQL = "";
-            JDBCResultRef jdbcResultRef = jdbcEngineExecutorServiceClientManager.query("starrocks", querySQL, engineExecutorRef);
-            System.out.println(jdbcResultRef);
-            log.info("index: " + i + ", duration: " + (jdbcResultRef.getJdbcOperationRef().getEndTime() - jdbcResultRef.getJdbcOperationRef().getStartTime()));
+            JDBCResultRef jdbcResultRef = null;
+            try {
+                jdbcResultRef = jdbcEngineExecutorServiceClientManager.query("starrocks", querySQL, engineExecutorRef);
+                log.info(jdbcResultRef.toString());
+                log.info("duration: " + (jdbcResultRef.getJdbcOperationRef().getEndTime() - jdbcResultRef.getJdbcOperationRef().getStartTime()));
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (TException e) {
+                e.printStackTrace();
+            }
+            return jdbcResultRef;
         }
-        System.out.println(System.currentTimeMillis() - startTime);
     }
 }
