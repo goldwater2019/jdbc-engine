585e6212ec5653766514d6d1ae57330ccdab945f
xinsen <xinsen.zhang.0571@gmail.com>
2022-04-08 23:16:16 +0800

    add: add build script

---
+++ build/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+JDBC_ENGINE_HOME=$(cd `dirname $0`/..;pwd)
+echo "JDBC ENGINE HOME: $JDBC_ENGINE_HOME"
+echo "CD TO $JDBC_ENGINE_HOME"
+cd $JDBC_ENGINE_HOME
+# mvn clean install package -DskipTests
+
+if [ ! -d "jar/" ];then
+  mkdir jar
+fi
+
+# cp
+cp jdbc-engine-common/target/*-jar-with-dependencies.jar jar/jdbc-engine-common.jar
+cp jdbc-engine-driver/target/*-jar-with-dependencies.jar jar/jdbc-engine-driver.jar
+cp jdbc-engine-executor/target/*-jar-with-dependencies.jar jar/jdbc-engine-executor.jar
