b0afbb145cc4de13ff9e9ff3085318d20ce0ce8e
xinsen <xinsen.zhang.0571@gmail.com>
2022-04-09 00:02:07 +0800

    update: detach pressure test with nohup

--- bin/start_pressure_test.sh
+++ bin/start_pressure_test.sh
@@ -2,5 +2,7 @@
 
 JDBC_ENGINE_HOME=$(cd `dirname $0`/..;pwd)
 
-java -cp $JDBC_ENGINE_HOME/jar/jdbc-engine-driver.jar \
-    com.ane56.engine.jdbc.pressure.DriverServerSideQuery $@
+nohup java -cp $JDBC_ENGINE_HOME/jar/jdbc-engine-driver.jar \
+    com.ane56.engine.jdbc.pressure.DriverServerSideQuery $@ \
+    $@ 2>&1 | tee logs/jdbc-engine-pressure-test-$USER.log & \
+    echo $! > pid/pressure_test.pid
