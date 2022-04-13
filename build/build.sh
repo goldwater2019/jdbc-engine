b4763f106320e6acf26c19d5daf8d139929b1fd2
xinsen <xinsen.zhang.0571@gmail.com>
2022-04-13 16:50:17 +0800

    bugfix: mvn core dump with dependency of gateway

--- build/build.sh
+++ build/build.sh
@@ -4,14 +4,14 @@ JDBC_ENGINE_HOME=$(cd `dirname $0`/..;pwd)
 echo "JDBC ENGINE HOME: $JDBC_ENGINE_HOME"
 echo "CD TO $JDBC_ENGINE_HOME"
 cd $JDBC_ENGINE_HOME
-# mvn clean install package -DskipTests
+ mvn clean install package -DskipTests
 
 if [ ! -d "jar/" ];then
   mkdir jar
 fi
 
 # cp
-cp jdbc-engine-common/target/*-jar-with-dependencies.jar jar/jdbc-engine-common.jar
+#cp jdbc-engine-common/target/*-jar-with-dependencies.jar jar/jdbc-engine-common.jar
 cp jdbc-engine-driver/target/*-jar-with-dependencies.jar jar/jdbc-engine-driver.jar
 cp jdbc-engine-executor/target/*-jar-with-dependencies.jar jar/jdbc-engine-executor.jar
 cp jdbc-engine-gateway/target/*.jar jar/jdbc-engine-gateway.jar
