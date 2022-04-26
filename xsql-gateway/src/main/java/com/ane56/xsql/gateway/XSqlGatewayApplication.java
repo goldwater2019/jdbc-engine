53f39ad4cd9b9a22cdf000f7b9df911caf842e3e
张鑫森 <zhangxinsen@ane56.com>
2022-04-26 11:14:39 +0800

    bugfix: upgrade curator to get rid from invalid event from zk

---
+++ xsql-gateway/src/main/java/com/ane56/xsql/gateway/XSqlGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.ane56.xsql.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zhangxinsen
+ * @Date: 2022/4/26 10:12 AM
+ * @Desc:
+ * @Version: v1.0
+ */
+
+@SpringBootApplication
+public class XSqlGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XSqlGatewayApplication.class, args);
+    }
+}
